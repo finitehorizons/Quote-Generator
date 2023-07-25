8b7cd482888d91b3c233f874e9fa2928cf325b3c
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-07-24 21:51:39 -0500

    added functionality

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-// Get Quotes from API
+// Global Variables
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if author field is blank and replace with "unknown"
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
+}
+
+// Get Quotes from API
 async function getQuotes() {
     const apiURL =
         "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
+        newQuote();
     } catch (err) {
         // catch error here
     }
